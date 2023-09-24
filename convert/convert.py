28499817b8534e19d1b03ec1131ad6fd5ecdf341
Alexander <alexyr.qa@gmail.com>
2023-09-24 23:05:28 +0300

    convert en

--- convert/convert.py
+++ convert/convert.py
@@ -1,15 +1,16 @@
 def unit_of_length():
     while True:
-        user_input = input("СМ - для перобразования сантиметров в дюймы. ДМ - для преобразования дюймов в сантиметры: ")
+        user_input = input("CM - to convert centimeters to inches. IN - to convert inches to centimeters: ")
         try:
-            if user_input in ("СМ", "ДМ"):
+            if user_input in ("CM", "IN"):
                 return user_input
         except ValueError:
             print("ERROR!")
 
+
 def getting_number_user():
     while True:
-        centimeters_str = input("Введите число: ")
+        centimeters_str = input("Enter a number: ")
         try:
             centimeters_float = float(centimeters_str)
             return centimeters_float
@@ -20,11 +21,11 @@ def getting_number_user():
 def convert_inches():
     user_input = unit_of_length()
     user_float = getting_number_user()
-    dm = 2.54
-    if user_input == "СМ":
-        print(user_float * dm)
-    elif user_input == "ДМ":
-        print(user_float / dm)
+    inch_to_cm = 2.54
+    if user_input == "CM":
+        print(f"{user_float} centimeter is {user_float / inch_to_cm} inch")
+    elif user_input == "IN":
+        print(f"{user_float} inch is {user_float * inch_to_cm} centimeter")
 
 
 convert_inches()
